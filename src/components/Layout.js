da86cfe973fb80501be86b9a96b20afe1889cedb
Ave-Mari <avemari98@gmail.com>
2022-11-05 14:14:19 +0300

    add span to show amount of goals

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link, Outlet } from "react-router-dom";
+import './../main.css';
 
-export default function Layout() {
+export default function Layout({goalsList}) {
   return (
     <>
     <header className="header">  
     <h1>Копилка 💰</h1> 
     <Link to="/">Главная</Link>
-    <Link to="goals-list">Мои цели</Link>
+    <Link to="goals-list">Мои цели <span className="list-number">{goalsList.length}</span></Link>
     <Link to="new-goal">Завести новую цель</Link>
     </header>
 
