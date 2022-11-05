917f3d16b641408a2b914b6793e9a072ac72f57b
Ave-Mari <avemari98@gmail.com>
2022-11-05 14:11:52 +0300

    add state functions to create array list of goals

--- src/App.js
+++ src/App.js
@@ -11,16 +11,57 @@ import Layout from "./components/Layout";
 
 export default function App() {
 
+  
+  const [goal, setGoal] = useState({
+    title: '',
+    total: ''
+  })
+
+  const [goalsList, setGoalsList] = useState([]);
+
+  
+  const inputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setGoal({...goal, [name]:value});
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (goal.title && goal.total) {
+      const newGoal = {...goal, id: new Date().valueOf() };
+      setGoalsList([...goalsList, newGoal]);
+      setGoal({title: '', total: ''});
+      console.log(goalsList)
+    }
+  }
+
   return (
     <>   
     <main>
   
   <section className="route">
   <Routes>
-    <Route path="/" element={<Layout />}>
+    <Route path="/" element={
+    <Layout 
+    goalsList={goalsList}
+    />}>
       <Route index element={<HomePage />}/>
-      <Route path="goals-list" element={<GoalsList />}/>
-      <Route path="new-goal" element={<NewGoal />}/>
+      <Route path="goals-list" element={
+      <GoalsList 
+      goalsList={goalsList}
+      />
+      }/>
+      <Route path="new-goal" element={
+      <NewGoal 
+      inputHandler={inputHandler} 
+      handleSubmit={handleSubmit} 
+      title={goal.title} 
+      total={goal.total}
+      />
+      }/>
       <Route path="*" element={ <NotFound /> }/>
       </Route>
     </Routes>
