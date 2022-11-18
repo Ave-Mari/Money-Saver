246b437df626edf5c90cbac4e44ec4fbf7880c30
Ave-Mari <avemari98@gmail.com>
2022-11-18 04:56:21 +0300

    add number of goals

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link, Outlet } from "react-router-dom";
 import './../main.css';
 
+
+
 export default function Layout({goalsList}) {
+
+  const lengthSpan = goalsList.length > 0 ? goalsList.length : '';
   return (
     <>
     <header className="header">  
     <h1>Копилка 💰</h1> 
     <Link to="/">Главная</Link>
-    <Link to="goals-list">Мои цели <span className="list-number">{goalsList.length}</span></Link>
+    <Link to="goals-list">Мои цели <span className="list-number">{lengthSpan}</span></Link>
     <Link to="new-goal">Завести новую цель</Link>
     </header>
 
