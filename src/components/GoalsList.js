7e0e22a34e65944e7eaa2faacf3cd0d77c3cf5cb
Ave-Mari <avemari98@gmail.com>
2022-11-18 04:57:02 +0300

    add props to add money and remove goals

--- src/components/GoalsList.js
+++ src/components/GoalsList.js
@@ -2,19 +2,31 @@ import React from 'react';
 import NewGoal from './NewGoal';
 import GoalItem from './GoalItem';
 
-export default function GoalsList( {goalsList} ) {
 
-  return (
+
+
+export default function GoalsList( { goalsList, addMoney, removeItem }) { 
+
+  
+   return (
     goalsList.map((item) => {
       const {title, total, id} = item;
-      return(
-        <ul key={id}>
-          <li>
-          <GoalItem item={item}/>
-          </li>
-        </ul>
+      return (
+          <ul  key={id}>          
+            <GoalItem 
+            total={total}
+            title={title}
+            addMoney={addMoney}
+            removeItem={removeItem}
+            id={id}
+              />      
+          </ul>
       )
+      
     })
-          
-  )
+   )
+   
+  
+
 }
+
