497fe83755e75023398e79adba8caef6a8f2e2b7
Ave-Mari <avemari98@gmail.com>
2022-11-18 04:57:54 +0300

    add type of total value

--- src/components/NewGoal.js
+++ src/components/NewGoal.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export default function NewGoal( { handleSubmit, inputHandler, title, total } ) {
+export default function NewGoal( {title, total, inputHandler, handleSubmit} ) {
 
   return (
     <>
@@ -8,7 +8,7 @@ export default function NewGoal( { handleSubmit, inputHandler, title, total } ) 
       <h2>Завести цель</h2>
       <form onSubmit={handleSubmit}>
       <input type="text" name="title" value={title} placeholder="Цель" onChange={inputHandler}/>
-      <input type="text" name="total" value={total} placeholder="Сумма" onChange={inputHandler}/>
+      <input type="text" name="total" value={total} placeholder="Сумма" pattern="[0-9]+" onChange={inputHandler}/>
       <button>Создать цель</button>
       </form>
     
