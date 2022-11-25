6d670a8f62d4eeaa1e5f5ddb86f1a2f59f6d3708
Ave-Mari <avemari98@gmail.com>
2022-11-25 22:49:49 +0300

    add labels to inputs

--- src/components/NewGoal.js
+++ src/components/NewGoal.js
@@ -4,11 +4,19 @@ export default function NewGoal( {title, total, inputHandler, handleSubmit} ) {
 
   return (
     <>
-    <section>
+    <section className='new-goal-section'>
       <h2>Завести цель</h2>
       <form onSubmit={handleSubmit}>
-      <input type="text" name="title" value={title} placeholder="Цель" onChange={inputHandler}/>
-      <input type="text" name="total" value={total} placeholder="Сумма" pattern="[0-9]+" onChange={inputHandler}/>
+        <label for="title" id="title-label">
+        {/* <span class="label-span">Ваша цель: </span> */}
+        <input id="title-input" type="text" name="title" value={title} placeholder="Ваша цель" onChange={inputHandler}/>
+          </label>        
+      
+      <label for="total" id="total-label">
+      {/* <span class="label-span">Сумма: </span> */}
+      <input id="total-input" type="text" name="total" value={total} placeholder="Сумма" pattern="[0-9]+" onChange={inputHandler}/>
+        </label>
+      
       <button>Создать цель</button>
       </form>
     
