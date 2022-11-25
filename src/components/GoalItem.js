a4f40d1386711a90882995fadbd979f6ae751e9b
Ave-Mari <avemari98@gmail.com>
2022-11-25 22:48:53 +0300

    add functions to finish users goal

--- src/components/GoalItem.js
+++ src/components/GoalItem.js
@@ -5,31 +5,90 @@ import NewGoal from './NewGoal';
 
 
 
-export default function GoalItem( { id, total, title, addMoney, removeItem } ) {
+export default function GoalItem( { 
+  id,
+  total, 
+  title, 
+  addMoney, 
+  removeItem, 
+  yourTotal, 
+  addYourTotal,
+  yourTotalHandler
+  } ) {
+
+    const [input, setInput] = useState(false);
+  
 
       return (
     <>
-     <li className='goal-item'>
+     <li className='goal-item'>      
       <button className='remove-goal-btn'
       onClick={() => removeItem(id)}
       >✖</button>
         <p>Моя цель: <span className='goal-span'>{title}</span> </p>
-        <p>Осталось собрать:  <span className='goal-span'>{total}₽</span></p>
+        <p>Осталось собрать:  <span className='goal-span'>{
+        total > 0 ? total : '0'
+        }
+        ₽
+        </span></p>
+        {
+          Number(total) > 0 ?
+
+          <div className='add-block'>
+        <p className='add-text'>Добавить: </p>
+        {
+          input ?
+          <div className="total-input">
+          <input 
+          type="text"
+          value={yourTotal}
+          onChange={(e) => yourTotalHandler(e)}
+          />
+        </div>
+        :
+      <div className='total-btns'>
         <input 
+       className='add-money-btn'
        type="button"
        value={1000}
        onClick={(e) => addMoney(id, total, e)}
        />
         <input 
+       className='add-money-btn'
        type="button"
        value={5000}
        onClick={(e) => addMoney(id, total, e)}
        />
         <input 
+       className='add-money-btn'
        type="button"
        value={10000}
        onClick={(e) => addMoney(id, total, e)}
        />
+        </div>
+        }
+        <input 
+       className='add-money-btn'
+       style={{width: '32px'}}
+       type="button"
+       value={input ? '✓' : '+'}
+       onClick={(e) => {
+        setInput(true);
+        if (input) {
+          addYourTotal(id, total, yourTotal, e, input);
+          setInput(false);
+         } 
+         
+       }}
+       />
+        </div>
+        :
+        <div>ВЫ НАКОПИЛИ ДЕНЕЖКИ!!! ПОЗДРАВЛЯЮЮЮ!!</div>
+
+          
+        }
+        
+       
 
 
     </li>
