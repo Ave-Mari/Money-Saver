ecdb37ef1e881ab29bc30e6aa2a391ca0d198d93
Ave-Mari <avemari98@gmail.com>
2022-11-25 22:47:23 +0300

    remove useless whitespaces

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,20 +3,19 @@ import { Link, Outlet } from "react-router-dom";
 import './../main.css';
 
 
-
 export default function Layout({goalsList}) {
 
-  const lengthSpan = goalsList.length > 0 ? goalsList.length : '';
+  const lengthSpan = goalsList.length > 0 ? goalsList.length : '0';
   return (
     <>
     <header className="header">  
     <h1>Копилка 💰</h1> 
     <Link to="/">Главная</Link>
-    <Link to="goals-list">Мои цели <span className="list-number">{lengthSpan}</span></Link>
+    <Link to="goals-list">Мои цели <span className={goalsList.length ? "list-number" : "list-number-zero"}>{lengthSpan}</span></Link>
     <Link to="new-goal">Завести новую цель</Link>
     </header>
-
     <Outlet />
+   
     </>
   )
 }
