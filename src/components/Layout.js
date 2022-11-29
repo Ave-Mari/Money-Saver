73e05e29c507f625ed5232c8dde668dcadedb35d
Ave-Mari <avemari98@gmail.com>
2022-11-29 21:54:25 +0300

    add burger menu button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Outlet } from "react-router-dom";
+import BurgerMenuLinks from './BurgerMenuLinks';
 import './../main.css';
 
 
 export default function Layout({goalsList}) {
 
+  const [burgerMenu, setBurgerMenu] = useState(false);
+
   const lengthSpan = goalsList.length > 0 ? goalsList.length : '0';
+  
   return (
     <>
     <header className="header">  
     <h1>Копилка 💰</h1> 
-    <Link to="/">Главная</Link>
-    <Link to="goals-list">Мои цели <span className={goalsList.length ? "list-number" : "list-number-zero"}>{lengthSpan}</span></Link>
-    <Link to="new-goal">Завести новую цель</Link>
+    <div className='desktop-links'>
+      <Link to="/">Главная</Link>
+      <Link to="goals-list">Мои цели <span className={goalsList.length ? "list-number" : "list-number-zero"}>{lengthSpan}</span></Link>
+      <Link to="new-goal">Завести новую цель</Link>
+     </div>
+    <button onClick={() => setBurgerMenu(!burgerMenu)}>
+    <a href="#" className='hamburger'>
+       <span classname="hamburger-line"></span>
+       <span classname="hamburger-line"></span>
+       <span classname="hamburger-line"></span>
+        </a>
+    </button>
     </header>
+
+    {burgerMenu ? <BurgerMenuLinks goalsList={goalsList}/> : ''}
     <Outlet />
    
     </>
