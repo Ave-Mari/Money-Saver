f1aa07ca89221f175a0a23a522dcdfda7bea6c55
Ave-Mari <avemari98@gmail.com>
2022-12-08 05:22:45 +0300

    add target to a tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import github from './../assets/github.svg'
 export default function Footer() {
   return (
     <footer className='footer'>
-<a href='https://github.com/Ave-Mari/Goal'>
+<a href='https://github.com/Ave-Mari/Goal' target="_blank">
     <img src={github} alt={'link to github'}></img>
     </a>   
     <p>2022</p>     
