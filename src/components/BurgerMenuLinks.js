b43a5b38ca54233ddacc58f91549e62e95670836
Ave-Mari <avemari98@gmail.com>
2022-12-08 22:28:33 +0300

    add x button

--- src/components/BurgerMenuLinks.js
+++ src/components/BurgerMenuLinks.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
-export default function BurgerMenuLinks({ goalsList }) {
+export default function BurgerMenuLinks({ goalsList, closeMobileNav }) {
 
     const lengthSpan = goalsList.length > 0 ? goalsList.length : '0';
 
     
   return (
     <nav className='nav'>
-      <button className='close-nav'>✖</button>
+      <button 
+      onClick={closeMobileNav}
+      className='close-nav'>
+        ✖
+        </button>
     <Link to="/">Главная</Link>
       <Link to="goals-list">Мои цели <span className={goalsList.length ? "list-number" : "list-number-zero"}>{lengthSpan}</span></Link>
       <Link to="new-goal">Завести новую цель</Link>
