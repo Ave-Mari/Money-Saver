95997bc07125f236b3fcdaa884c167e6a69a865b
Mari Vaypan <avemari98@gmail.com>
2023-01-12 18:52:41 +0300

    create component to change lang of page

---
+++ src/components/LanguageSwitcher.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function LanguageSwitcher() {
+  return (
+        <center>
+	<div class="switch">
+	    <input id="language-toggle" class="check-toggle check-toggle-round-flat" type="checkbox"/>
+	    <label for="language-toggle"></label>
+	    <span class="on">🇷🇺</span>
+	    <span class="off">🇬🇧</span>
+  	</div>
+ </center>
+  )
+}
