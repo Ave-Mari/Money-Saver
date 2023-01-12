fac595fe772e6be563b16da05691f28562f0520b
Mari Vaypan <avemari98@gmail.com>
2023-01-13 00:37:25 +0300

    fix styles of switcher

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import './../language-switcher.css'
+import './../language-switcher.css';
+
 
 export default function LanguageSwitcher() {
+    //  const a = document.querySelector('.check-toggle-round-flat');
+    //  console.log(a);
+    // // console.log(a.innerHTML);
+
+    // if (a.i)
+    const getChecked = (e) => {
+        console.log(e.getAttribute(checked));
+    }
   return (
         <center>
 	<div class="switch">
 	    <input id="language-toggle" 
-        class="check-toggle 
-        check-toggle-round-flat" 
+        class="check-toggle check-toggle-round-flat" 
         type="checkbox"
+        checked
+        onChange={(e) => getChecked(e)}
         />
 	    <label for="language-toggle"></label>
 	    <span class="ru">🇷🇺</span>
