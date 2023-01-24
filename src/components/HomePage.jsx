37aec0ff781f3cbf2d74a2a3ac652f1f3278f2ac
Mari Vaypan <avemari98@gmail.com>
2023-01-25 01:33:50 +0300

    fix string

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage({isRussian}) {
       {isRussian ? 
       "Это Копилка, которая позволяет вам постепенно копить деньги на желаемую цель." 
       : 
-      "This Goal App will help you to collect money for your goals step-by-step. Just create a New Goal using "New Goal" tab. Then you can add amounts using default buttons or type your own amount. When you finish collecting, your goal will mark as finished goal."
+      "This Goal App will help you to collect money for your goals step-by-step. Just create a New Goal using \"New Goal\" tab. Then you can add amounts using default buttons or type your own amount. When you finish collecting, your goal will mark as finished goal."
       }
       
       </div>
