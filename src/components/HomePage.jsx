6af0261f480a582bcaef7305e6d486c3fc9ebff5
Mari Vaypan <avemari98@gmail.com>
2023-01-25 01:28:47 +0300

    add language switcher to component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function HomePage() {
+export default function HomePage({isRussian}) {
   return (
-    <div>Это Копилка, которая позволяет вам постепенно копить деньги на желаемую цель.</div>
+    <div>
+      {isRussian ? 
+      "Это Копилка, которая позволяет вам постепенно копить деньги на желаемую цель." 
+      : 
+      "This Goal App will help you to collect money for your goals step-by-step. Just create a New Goal using "New Goal" tab. Then you can add amounts using default buttons or type your own amount. When you finish collecting, your goal will mark as finished goal."
+      }
+      
+      </div>
   )
 }
