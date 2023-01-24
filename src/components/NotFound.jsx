f716562f70f2d2c7d2655b9d93f955ec7f4aed18
Mari Vaypan <avemari98@gmail.com>
2023-01-25 01:29:05 +0300

    add language switcher to component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function NotFound() {
+export default function NotFound({isRussian}) {
   return (
-    <div className='not-found'>Такой страницы не сущетсвует</div>
+    <div className='not-found'>
+      {isRussian ? "Такой страницы не сущетсвует" : "This page doesn't exist"}    
+      </div>
   )
 }
