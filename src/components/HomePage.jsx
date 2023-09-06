412b6692e650388507434548992b3bc70bde4043
Mari Vaypane <avemari98@gmail.com>
2023-09-06 15:21:35 +0300

    add description to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,9 +4,9 @@ export default function HomePage({isRussian}) {
   return (
     <div>
       {isRussian ? 
-      "Это Копилка, которая позволяет вам постепенно копить деньги на желаемую цель." 
+      "Это приложение Копилка, которая позволяет вам постепенно копить деньги на желаемую цель. Создайте новую цель, используя вкладку \"Завести новую цель\". Затем вы можете добавлять сумму, используя стандартные кпоки или вписав свою собственную сумму. Когда вы закончите сбор, ваша цель пометится как завершенная." 
       : 
-      "This Goal App will help you to collect money for your goals step-by-step. Just create a New Goal using \"New Goal\" tab. Then you can add amounts using default buttons or type your own amount. When you finish collecting, your goal will mark as finished goal."
+      "This Money Saver App will help you to collect money for your goals step-by-step. Just create a New Goal using \"New Goal\" tab. Then you can add amounts using default buttons or type your own amount. When you finish collecting, your goal will mark as finished goal."
       }
       
       </div>
